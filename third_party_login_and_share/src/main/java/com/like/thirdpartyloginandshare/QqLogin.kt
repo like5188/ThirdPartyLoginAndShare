cd097080a7a336c3f8f60a1a25a76d906506e601
PC-20180923QFUS\Administrator <liuhongyun@adtech.com.cn>
2018-12-27 14:11:03 +0800

    QQ登录、分享

--- third_party_login_and_share/src/main/java/com/like/thirdpartyloginandshare/QqLogin.kt
+++ third_party_login_and_share/src/main/java/com/like/thirdpartyloginandshare/QqLogin.kt
@@ -4,109 +4,153 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.support.v4.app.Fragment
+import android.text.TextUtils
+import com.tencent.connect.UnionInfo
+import com.tencent.connect.UserInfo
 import com.tencent.connect.common.Constants
-import com.tencent.tauth.IRequestListener
 import com.tencent.tauth.IUiListener
 import com.tencent.tauth.Tencent
 import com.tencent.tauth.UiError
-
+import org.json.JSONObject
 
 /**
  * QQ登录工具类
  * 应用需要在调用接口的Activity的onActivityResult方法中调用[onActivityResult]
- *
- * 登录常见错误码信息：
- * 110201：未登陆
- * 110405：登录请求被限制
- * 110404：请求参数缺少appid
- * 110401：请求的应用不存在
- * 110407：应用已经下架
- * 110406：应用没有通过审核
- * 100044：错误的sign
- * 110500：获取用户授权信息失败
- * 110501：获取应用的授权信息失败
- * 110502：设置用户授权失败
- * 110503：获取token失败
- * 110504：系统内部错误
  */
-class QqLogin() {
-
-    private val mUiListener = object : IUiListener {
-        /*
-        {
-            "ret":0,
-            "pay_token":"xxxxxxxxxxxxxxxx",
-            "pf":"openmobile_android",
-            "expires_in":"7776000",
-            "openid":"xxxxxxxxxxxxxxxxxxx",
-            "pfkey":"xxxxxxxxxxxxxxxxxxx",
-            "msg":"sucess",
-            "access_token":"xxxxxxxxxxxxxxxxxxxxx"
-        }
-         */
-        override fun onComplete(p0: Any?) {
-        }
-
-        override fun onCancel() {
-        }
-
-        override fun onError(p0: UiError?) {
-        }
-    }
+class QqLogin(context: Context, private val mLoginListener: LoginListener) {
+    private val mTencent = Tencent.createInstance(QQ_APP_ID, context.applicationContext)
 
     fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == Constants.REQUEST_LOGIN) {
-            Tencent.onActivityResultData(requestCode, resultCode, data, mUiListener)
+            Tencent.onActivityResultData(requestCode, resultCode, data, mLoginListener)
         }
     }
 
     fun login(activity: Activity) {
         checkSessionValidAndLogin(activity) {
-            it.login(activity, "all", mUiListener)
+            mTencent.login(activity, "all", mLoginListener)
         }
     }
 
     fun login(fragment: Fragment) {
         checkSessionValidAndLogin(fragment.context) {
-            it.login(fragment, "all", mUiListener)
+            mTencent.login(fragment, "all", mLoginListener)
         }
     }
 
-    private inline fun checkSessionValidAndLogin(context: Context?, login: (Tencent) -> Unit) {
-        Tencent.createInstance(QQ_APP_ID, context?.applicationContext)?.let {
-            if (it.checkSessionValid(QQ_APP_ID)) {
-                it.initSessionCache(it.loadSession(QQ_APP_ID))
-            } else {// token过期，请调用登录接口拉起手Q授权登录
-                login(it)
-            }
-        }
-    }
+    fun isLogin(context: Context) = mTencent.isSessionValid
 
     fun logout(context: Context) {
-        Tencent.createInstance(QQ_APP_ID, context.applicationContext)?.logout(context)
+        mTencent.logout(context)
     }
 
-    /*
-    {
-        "is_yellow_year_vip": "0",
-        "ret": 0,
-        "figureurl_qq_1": "http://q.qlogo.cn/qqapp/222222/8C75BBE3DC6B0E9A64BD31449A3C8CB0/40",
-        "figureurl_qq_2": "http://q.qlogo.cn/qqapp/222222/8C75BBE3DC6B0E9A64BD31449A3C8CB0/100",
-        "nickname": "小罗",
-        "yellow_vip_level": "0",
-        "msg": "",
-        "figureurl_1": "http://qzapp.qlogo.cn/qzapp/222222/8C75BBE3DC6B0E9A64BD31449A3C8CB0/50",
-        "vip": "0",
-        "level": "0",
-        "figureurl_2": "http://qzapp.qlogo.cn/qzapp/222222/8C75BBE3DC6B0E9A64BD31449A3C8CB0/100",
-        "is_yellow_vip": "0",
-        "gender": "男",
-        "figureurl": "http://qzapp.qlogo.cn/qzapp/222222/8C75BBE3DC6B0E9A64BD31449A3C8CB0/30"
+    fun getUserInfo(context: Context, listener: GetUserInfoListener) {
+        if (isLogin(context)) {
+            UserInfo(context, mTencent.qqToken).getUserInfo(listener)
+        } else {
+            listener.onFailure("获取用户信息失败 尚未登录")
+        }
     }
-     */
-    fun getUserInfo(context: Context, listener: IRequestListener) {
-//        Tencent.createInstance(QQ_APP_ID, context.applicationContext)
-//            ?.requestAsync(Constants.GRAPH_SIMPLE_USER_INFO, null, Constants.HTTP_GET, listener, null)
+
+    fun getUnionId(context: Context, listener: GetUnionIdListener) {
+        if (isLogin(context)) {
+            UnionInfo(context, mTencent.qqToken).getUnionId(listener)
+        } else {
+            listener.onFailure("获取unionId失败 尚未登录")
+        }
+    }
+
+    private inline fun checkSessionValidAndLogin(context: Context?, login: () -> Unit) {
+        if (mTencent.checkSessionValid(QQ_APP_ID)) {
+            mTencent.initSessionCache(mTencent.loadSession(QQ_APP_ID))
+            mLoginListener.onSuccess()
+        } else {// token过期，请调用登录接口拉起手Q授权登录
+            login()
+        }
+    }
+
+    abstract class GetUserInfoListener : IUiListener {
+        override fun onComplete(response: Any?) {
+            val jsonObject = response as JSONObject?
+            if (null != jsonObject && jsonObject.length() != 0) {
+                onSuccess(jsonObject)
+            } else {
+                onFailure("获取用户信息失败 返回为空")
+            }
+        }
+
+        override fun onCancel() {
+            onFailure("取消获取用户信息")
+        }
+
+        override fun onError(e: UiError?) {
+            onFailure("获取用户信息失败 ${e?.errorDetail}")
+        }
+
+        abstract fun onSuccess(jsonObject: JSONObject)
+
+        abstract fun onFailure(errorMessage: String)
+    }
+
+    abstract class GetUnionIdListener : IUiListener {
+        override fun onComplete(response: Any?) {
+            val jsonObject = response as JSONObject?
+            if (null != jsonObject && jsonObject.length() != 0) {
+                try {
+                    onSuccess(jsonObject.getString("unionid"))
+                } catch (e: Exception) {
+                    onFailure("获取unionId成功 但是解析数据失败")
+                }
+            } else {
+                onFailure("获取unionId失败 返回为空")
+            }
+        }
+
+        override fun onCancel() {
+            onFailure("取消获取unionId")
+        }
+
+        override fun onError(e: UiError?) {
+            onFailure("获取unionId失败 ${e?.errorDetail}")
+        }
+
+        abstract fun onSuccess(unionId: String)
+
+        abstract fun onFailure(errorMessage: String)
+    }
+
+    abstract inner class LoginListener : IUiListener {
+
+        override fun onComplete(response: Any?) {
+            val jsonObject = response as JSONObject?
+            if (null != jsonObject && jsonObject.length() != 0) {
+                try {
+                    val token = jsonObject.getString(Constants.PARAM_ACCESS_TOKEN)
+                    val expires = jsonObject.getString(Constants.PARAM_EXPIRES_IN)
+                    val openId = jsonObject.getString(Constants.PARAM_OPEN_ID)
+                    if (!TextUtils.isEmpty(token) && !TextUtils.isEmpty(expires) && !TextUtils.isEmpty(openId)) {
+                        mTencent.setAccessToken(token, expires)
+                        mTencent.openId = openId
+                    }
+                } catch (e: Exception) {
+                }
+                onSuccess()
+            } else {
+                onFailure("登录失败 返回为空")
+            }
+        }
+
+        override fun onError(e: UiError) {
+            onFailure("登录失败 ${e.errorDetail}")
+        }
+
+        override fun onCancel() {
+            onFailure("取消登录")
+        }
+
+        abstract fun onSuccess()
+
+        abstract fun onFailure(errorMessage: String)
     }
 
 }
