fbf0461f40dededa367cdb561f6e7e90a52eaf9f
PC-20180923QFUS\Administrator <liuhongyun@adtech.com.cn>
2018-12-27 14:57:26 +0800

    QQ登录、分享

--- third_party_login_and_share/src/main/java/com/like/thirdpartyloginandshare/QqZoneShare.kt
+++ third_party_login_and_share/src/main/java/com/like/thirdpartyloginandshare/QqZoneShare.kt
@@ -56,8 +56,15 @@ class QqZoneShare(private val activity: Activity, private val mShareListener: IU
      *
      * @param summary           说说正文（传图和传视频接口会过滤第三方传过来的自带描述，目的为了鼓励用户自行输入有价值信息）
      * @param imageUrl          说说的图片, 以ArrayList<String>的类型传入，以便支持多张图片（注：<=9张图片为发表说说，>9张为上传图片到相册），只支持本地图片
+     * @param scene             区分分享场景，用于异化feeds点击行为和小尾巴展示
+     * @param callback          游戏自定义字段，点击分享消息回到游戏时回传给游戏
      */
-    fun publishMood(summary: String = "", imageUrl: ArrayList<String>? = null) {
+    fun publishMood(
+        summary: String = "",
+        imageUrl: ArrayList<String>? = null,
+        scene: String = "",
+        callback: String = ""
+    ) {
         val params = Bundle()
         // 分享的类型
         // QzonePublish.PUBLISH_TO_QZONE_TYPE_PUBLISHMOOD（发表说说、上传图片）
@@ -67,6 +74,14 @@ class QqZoneShare(private val activity: Activity, private val mShareListener: IU
         params.putString(QzonePublish.PUBLISH_TO_QZONE_SUMMARY, summary)
         // 说说的图片, 以ArrayList<String>的类型传入，以便支持多张图片（注：<=9张图片为发表说说，>9张为上传图片到相册），只支持本地图片
         params.putStringArrayList(QzonePublish.PUBLISH_TO_QZONE_IMAGE_URL, imageUrl)
+
+        val extParams = Bundle()
+        // 区分分享场景，用于异化feeds点击行为和小尾巴展示
+        extParams.putString(QzonePublish.HULIAN_EXTRA_SCENE, scene)
+        // 游戏自定义字段，点击分享消息回到游戏时回传给游戏
+        extParams.putString(QzonePublish.HULIAN_CALL_BACK, callback)
+        params.putBundle(QzonePublish.PUBLISH_TO_QZONE_EXTMAP, extParams)
+
         mTencent.publishToQzone(activity, params, mShareListener)
     }
 
@@ -74,8 +89,14 @@ class QqZoneShare(private val activity: Activity, private val mShareListener: IU
      * 发表视频
      *
      * @param videoLocalPath    发表的视频，只支持本地地址，发表视频时必填；上传视频的大小最好控制在100M以内
+     * @param scene             区分分享场景，用于异化feeds点击行为和小尾巴展示
+     * @param callback          游戏自定义字段，点击分享消息回到游戏时回传给游戏
      */
-    fun publishVideo(videoLocalPath: String) {
+    fun publishVideo(
+        videoLocalPath: String,
+        scene: String = "",
+        callback: String = ""
+    ) {
         val params = Bundle()
         // 分享的类型
         // QzonePublish.PUBLISH_TO_QZONE_TYPE_PUBLISHMOOD（发表说说、上传图片）
@@ -84,6 +105,14 @@ class QqZoneShare(private val activity: Activity, private val mShareListener: IU
         // 发表的视频，只支持本地地址，发表视频时必填；上传视频的大小最好控制在100M以内
         // （因为QQ普通用户上传视频必须在100M以内，黄钻用户可上传1G以内视频，大于1G会直接报错。）
         params.putString(QzonePublish.PUBLISH_TO_QZONE_VIDEO_PATH, videoLocalPath)
+
+        val extParams = Bundle()
+        // 区分分享场景，用于异化feeds点击行为和小尾巴展示
+        extParams.putString(QzonePublish.HULIAN_EXTRA_SCENE, scene)
+        // 游戏自定义字段，点击分享消息回到游戏时回传给游戏
+        extParams.putString(QzonePublish.HULIAN_CALL_BACK, callback)
+        params.putBundle(QzonePublish.PUBLISH_TO_QZONE_EXTMAP, extParams)
+
         mTencent.publishToQzone(activity, params, mShareListener)
     }
 
