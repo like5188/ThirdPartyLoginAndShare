34a7df8da5e8deb4bf74e84a3d67982d12ac231b
PC-20180923QFUS\Administrator <liuhongyun@adtech.com.cn>
2018-12-28 11:57:46 +0800

    重构

---
+++ third_party_login_and_share/src/main/java/com/like/thirdpartyloginandshare/ThirdPartyShare.kt
@@ -0,0 +1,57 @@
+package com.like.thirdpartyloginandshare
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v4.app.Fragment
+import com.like.thirdpartyloginandshare.share.*
+import com.like.thirdpartyloginandshare.util.PlatForm
+import com.like.thirdpartyloginandshare.util.SingletonHolder
+import kotlin.jvm.functions.FunctionN
+
+class ThirdPartyShare private constructor(private val mActivity: Activity) {
+    companion object : SingletonHolder<ThirdPartyShare>(object : FunctionN<ThirdPartyShare> {
+        override val arity: Int = 1 // number of arguments that must be passed to constructor
+
+        override fun invoke(vararg args: Any?): ThirdPartyShare {
+            return ThirdPartyShare(args[0] as Activity)
+        }
+    }) {
+        fun with(activity: Activity): ThirdPartyShare = getInstance(activity)
+
+        fun with(fragment: Fragment): ThirdPartyShare = getInstance(fragment.activity)
+    }
+
+    private lateinit var mStrategy: ShareStrategy
+
+    fun setPlatForm(platForm: PlatForm): ThirdPartyShare {
+        when (platForm) {
+            PlatForm.QQ -> {
+                mStrategy = QqShare(mActivity)
+            }
+            PlatForm.QZONE -> {
+                mStrategy = QqZoneShare(mActivity)
+            }
+            PlatForm.WEIXIN -> {
+                mStrategy = WxShare(mActivity)
+            }
+            PlatForm.WEIXIN_CIRCLE -> {
+                mStrategy = WxCircleShare(mActivity)
+            }
+            PlatForm.SINA -> {
+                mStrategy = WbShare(mActivity)
+            }
+        }
+        return this
+    }
+
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        checkParams()
+        mStrategy.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun checkParams() {
+        if (!::mStrategy.isInitialized) {
+            throw UnsupportedOperationException("请先调用setPlatForm(platForm: PlatForm)方法进行初始化")
+        }
+    }
+}
