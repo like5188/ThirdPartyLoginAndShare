c1b742ff71b10ea728fda58f27c5dac01892c0ef
like <like@adtech.com.cn>
2020-01-10 10:02:51 +0800

    完善

--- third_party_login_and_share/src/main/java/com/like/thirdpartyloginandshare/share/params/imageandtext/QqImageAndTextParams.kt
+++ third_party_login_and_share/src/main/java/com/like/thirdpartyloginandshare/share/params/imageandtext/QqImageAndTextParams.kt
@@ -3,11 +3,17 @@ package com.like.thirdpartyloginandshare.share.params.imageandtext
 import com.like.thirdpartyloginandshare.share.params.ShareParams
 
 /**
+ * 图文消息
+ *
  * @param title         分享的标题, 最长30个字符。
  * @param targetUrl     这条分享消息被好友点击后的跳转URL
  * @param summary       分享的消息摘要，最长40个字。
  * @param imageUrl      分享图片的URL或者本地路径
- * @param arkStr        Ark JSON串
+ * @param arkStr        ARK JSON格式字符串。
+ * app:ARK应用名称，com.tencent.map
+ * view:ARK应用对应展示的视图
+ * meta:ARK应用元数据，view视图对应需要的JSON字符串元数据
+ * {"app":"com.dianping.dpscope","view":"RestaurantShare","meta":{"ShopData":{"shopId":"2511961"}}}
  */
 data class QqImageAndTextParams(
     val title: String,
--- third_party_login_and_share/src/main/java/com/like/thirdpartyloginandshare/share/params/program/QqProgramParams.kt
+++ third_party_login_and_share/src/main/java/com/like/thirdpartyloginandshare/share/params/program/QqProgramParams.kt
@@ -3,20 +3,23 @@ package com.like.thirdpartyloginandshare.share.params.program
 import com.like.thirdpartyloginandshare.share.params.ShareParams
 
 /**
- * @param title         分享的标题, 最长30个字符。
- * @param targetUrl     这条分享消息被好友点击后的跳转URL
- * @param summary       分享的消息摘要，最长40个字。
- * @param imageUrl      分享图片的URL或者本地路径
- * @param programAppId
- * @param programPath
- * @param programType
+ * QQ小程序
+ * 可以从外部app分享到手Q为一个QQ小程序，并可以指定预览图、文案、小程序路径
+ *
+ * @param title         分享的标题, 最长30个字符。如果不填，默认使用小程序名称作为标题
+ * @param targetUrl     兼容低版本的网页连接
+ * @param summary       分享的消息摘要，最长40个字。如果不填，默认使用小程序后台注册的描述作为摘要
+ * @param imageUrl      分享预览封面图片的URL或者本地路径
+ * @param programAppId  分享的小程序appId，小程序与当前应用必须为同一个主体
+ * @param programPath   分享的小程序页面路径，如果不需要指定，请填主页路径
+ * @param programType   3表示正式版，1表示体验版
  */
 data class QqProgramParams(
-    val title: String,
+    val title: String = "",
     val targetUrl: String,
     val summary: String = "",
-    val imageUrl: String = "",
-    val programAppId: String = "",
-    val programPath: String = "",
+    val imageUrl: String,
+    val programAppId: String,
+    val programPath: String,
     val programType: String = ""
 ) : ShareParams
