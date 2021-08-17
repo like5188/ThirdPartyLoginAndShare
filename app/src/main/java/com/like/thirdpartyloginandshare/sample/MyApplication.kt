188fb8926853ce901a8cd8b23d17c220b7791c55
like5188 <260864866@qq.com>
2021-08-17 09:39:27 +0800

    友盟社会化分享集成

--- app/src/main/java/com/like/thirdpartyloginandshare/sample/MyApplication.kt
+++ app/src/main/java/com/like/thirdpartyloginandshare/sample/MyApplication.kt
@@ -13,17 +13,17 @@ class MyApplication : Application() {
         super.onCreate()
 
         UMConfigure.init(
-            this, "5a12384aa40fa3551f0001d1", "umeng", UMConfigure.DEVICE_TYPE_PHONE, ""
+            this, "592b8268e88bad10320009ca", "umeng", UMConfigure.DEVICE_TYPE_PHONE, ""
         )//58edcfeb310c93091c000be2 5965ee00734be40b580001a0
 
         // 微信设置
-        PlatformConfig.setWeixin("wxa9cce595f2c0b87b", "3baf1193c85774b3fd9d18447d76cab0");
+        PlatformConfig.setWeixin("wxa9cce595f2c0b87b", "979610c4b66148e4dd7a46f25f261058");
         PlatformConfig.setWXFileProvider("com.like.thirdpartyloginandshare.fileprovider");
         // QQ设置
-        PlatformConfig.setQQZone("101540498", "5d63ae8858f1caab67715ccd6c18d7a5");
+        PlatformConfig.setQQZone("101540498", "e957a6694ea10bead40ebc7301275eca");
         PlatformConfig.setQQFileProvider("com.like.thirdpartyloginandshare.fileprovider");
         // 新浪微博设置
-        PlatformConfig.setSinaWeibo("1929959086", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
+        PlatformConfig.setSinaWeibo("1929959086", "376628131f4e598e7b13b682bab35f91", "http://sns.whalecloud.com");
         PlatformConfig.setSinaFileProvider("com.like.thirdpartyloginandshare.fileprovider");
 
         // 设置每次登录拉取确认界面（目前SDK默认设置为在Token有效期内登录不进行二次授权）
